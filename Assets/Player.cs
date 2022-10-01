6e1df690126fc8804c950423c3c1cd969dceab56
Nguyen Dang Truong Anh <truonganh20062001@gmail.com>
2022-10-01 18:40:28 +0700

    First movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D myRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     private void Run()
     {
-
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
     }
 
 }
