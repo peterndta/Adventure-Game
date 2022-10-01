a3b979c771c4b535b5e2a8363c5ae71226d8f0f6
Nguyen Dang Truong Anh <truonganh20062001@gmail.com>
2022-10-02 00:06:37 +0700

    Character basic movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
+    // Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
+    // State
+    bool isAlive = true;
+    bool isJumping = false;
+
+    // Cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -26,12 +38,31 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        
+        bool playerMoveLeftRight = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Run", playerMoveLeftRight);
     }
 
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            //Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            //myRigidBody.velocity += jumpVelocity;
+            myRigidBody.velocity = Vector2.zero;
+            Vector2 jumpVelocity = new Vector2(0, jumpSpeed);
+            myRigidBody.AddForce(jumpVelocity, ForceMode2D.Impulse);
+        }
+    }
     private void FlipSprite()
     {
         bool playerMoveLeftRight = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        if(playerMoveLeftRight)
+        if (playerMoveLeftRight)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
         }
