9164bc35ccf19c2dfb5e191fa98c7f3ed7c1ea13
Nguyen Dang Truong Anh <truonganh20062001@gmail.com>
2022-10-05 21:16:41 +0700

    Add remembering destroy coin when dying

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+    int startSceneIndex;
+
+    private void Awake()
+    {
+        int numScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if (numScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex != startSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
